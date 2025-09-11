bcdbe6927fdb1f2ae5d4d0ee51ab7e9f9c5862b6
claire-mayfield <claire_mayfield@icloud.com>
2025-09-10 22:37:22 -0400

    added script

---
+++ ControlFlow_WorkingScript/ControlFlow_WorkingScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+public class ControlFlow : MonoBehaviour
+{
+    public bool flag;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log(flag);
+        if (flag == true)
+        {
+            Debug.Log("Boolean flag is set");
+        }
+        else
+        {
+            Debug.Log("Boolean flag isn't set");
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            float Result;
+            Result = Mathf.Pow(2, i);
+            Debug.Log("The " + i + " power of " + 2 + " is " + Result);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
